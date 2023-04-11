276a3721a2899ea2c1da1a469d6eb1db38574ad8
Vladimir Zlobinsky <vladimir.zlobinsky@nordlb.de>
2023-04-11 14:37:25 +0200

    separate entity/dto

--- src/main/java/de/vovchello/quarkus/internal/db/internal/TaxiInMemoryDb.java
+++ src/main/java/de/vovchello/quarkus/internal/db/internal/TaxiInMemoryDb.java
@@ -1,5 +1,7 @@
 package de.vovchello.quarkus.internal.db.internal;
 
+import static de.vovchello.quarkus.internal.db.internal.TaxiEntityConverter.taxiEntityFromTaxi;
+
 import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
@@ -7,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
+import java.util.stream.Collectors;
 
 import javax.enterprise.context.ApplicationScoped;
 
@@ -24,17 +27,17 @@ import io.quarkus.runtime.Startup;
 class TaxiInMemoryDb implements ReadTaxi, WriteTaxi {
     private final static Logger LOGGER = Logger.getLogger(TaxiInMemoryDb.class);
 
-    private Map<String, Taxi> db = new ConcurrentHashMap<>();
+    private Map<String, TaxiEntity> db = new ConcurrentHashMap<>();
 
     private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
     TaxiInMemoryDb() {
         LOGGER.info("creation");
-        Taxi taxi1 = new Taxi("1", "taxi1", true);
+        TaxiEntity taxi1 = new TaxiEntity("1", "taxi1", true);
         db.put(taxi1.getId(), taxi1);
-        Taxi taxi2 = new Taxi("2", "taxi2", true);
+        TaxiEntity taxi2 = new TaxiEntity("2", "taxi2", true);
         db.put(taxi2.getId(), taxi2);
-        Taxi taxi3 = new Taxi("3", "taxi3", false);
+        TaxiEntity taxi3 = new TaxiEntity("3", "taxi3", false);
         db.put(taxi3.getId(), taxi3);
     }
 
@@ -43,9 +46,12 @@ class TaxiInMemoryDb implements ReadTaxi, WriteTaxi {
     public Optional<Taxi> getTaxiById(String id) {
         try {
             MDC.put("itemId", id);
-
             LOGGER.infof("get item by id %s", id);
-            return Optional.ofNullable(db.get(id));
+
+            if (!db.containsKey(id)) {
+                return Optional.empty();
+            }
+            return Optional.ofNullable(TaxiEntityConverter.taxiEntityToTaxi(db.get(id)));
         } finally {
             MDC.remove("itemId");
         }
@@ -60,19 +66,23 @@ class TaxiInMemoryDb implements ReadTaxi, WriteTaxi {
     @Override
     public Set<Taxi> getAllTaxies() {
         LOGGER.infof("get all items");
-        return Set.copyOf(db.values());
+        return db.values().stream().map(TaxiEntityConverter::taxiEntityToTaxi).collect(Collectors.toSet());
     }
 
     @Override
     public void addTaxi(Taxi taxi) {
-        db.putIfAbsent(taxi.getId(), taxi);
+        if (!db.containsKey(taxi.getId())) {
+            TaxiEntity entity = taxiEntityFromTaxi(taxi);
+            db.put(entity.getId(), entity);
+        }
     }
 
     @Override
     public void updateTaxi(Taxi taxi) {
         LOGGER.infof("update taxi %s", taxi);
         if (db.containsKey(taxi.getId())) {
-            db.put(taxi.getId(), taxi);
+            TaxiEntity entity = taxiEntityFromTaxi(taxi);
+            db.put(entity.getId(), entity);
         }
     }
 
